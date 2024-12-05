6c2743b44046e4e5b3fd485234b22f1746e3a1af
Rilbe <Rilbe@LAPTOP-7O24V1AT.srunet.sruad.edu>
2024-12-05 11:21:30 -0500

    Commit

---
+++ Animals/src/Elephant.java
@@ -0,0 +1,80 @@
+
+public class Elephant {
+	
+	
+	//attributes
+	private String trunk;
+	private int legs;
+	private String ears;
+	private int tusks;
+	private String color;
+	private boolean fly;
+	
+	//methods
+	void setTrunk (String trunkLength)
+	{
+		trunk = trunkLength;
+	}
+	String getTrunk()
+	{
+		return trunk;
+	}
+	
+	void setLegs (int noLegs)
+	{
+		legs = noLegs;
+	}
+	int getLegs()
+	{
+		return legs;
+	}
+	
+	void setEars (String earSize)
+	{
+		ears = earSize;
+	}
+	String getEars()
+	{
+		return ears;
+	}
+	
+	void setTusks(int noTusks)
+	{
+		tusks = noTusks;
+	}
+	int getTusks()
+	{
+		return tusks;
+	}
+	
+	void setColor (String bodyColor)
+	{
+		color = bodyColor;
+	}
+	String getColor()
+	{
+		return color;
+	}
+	
+	void setFly (boolean canFly)
+	{
+		fly = canFly;
+	}
+	boolean getFly()
+	{
+		return fly;
+	}
+
+	public static void main (String [] args)
+	{
+		//Clyde is an instance of an Elephant
+		Elephant clyde = new Elephant();		
+		clyde.setLegs(4);
+		System.out.print("Clyde has " +clyde.getLegs()+" legs" );
+		
+		Elephant brewster = new Elephant();		
+		brewster.setLegs(6);
+		System.out.print("\nBrewster has " +brewster.getLegs()+" legs" );
+	}
+
+}
